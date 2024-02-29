6b1a7c02ba94b997618e9a1b1981f81d61ed0c2f
Bappy dase <bappydas.bd51@gmail.com>
2024-02-29 16:26:27 +0600

    Intialiazed the pos app

---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const navbar = () => {
+  return <div>ghj</div>;
+};
+
+export default navbar;
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const homePage = () => {
+  return (
+    <div className="bg-amber-500 text-zinc-600">
+      <h1>This is Home Page For this pos app</h1>
+    </div>
+  );
+};
+
+export default homePage;
